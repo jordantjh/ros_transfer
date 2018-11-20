822d64ceda741c8a1326dc2d6d246dfa04ab5c41
jordantjh <jordantjh@hotmail.com>
2018-11-20 07:48:18 -0600

    completed .ino haven't verified

--- varyingMotorSpeed/varyingMotorSpeed.ino
+++ varyingMotorSpeed/varyingMotorSpeed.ino
@@ -1,23 +1,56 @@
+#include <ros.h>
+#include <std_msgs/String.h>
+
 #define delay_fast 2
 #define delay_slow_down 7
 
-void setup() {
-  // put your setup code here, to run once:
+ros::NodeHandle  nh;
+String latest_color = "green";  // default to "green" as well
+int cur_stepper_val;
+
+void messageCb( const std_msgs::String& color_published){
+  latest_color = color_published;   // blink the led
 }
 
+void setup() {
+  // put your setup code here, to run once:
+  nh.initNode();
+  nh.subscribe(command_card_t);   // command_card_t is the topic to subscribe to
+}
+
+//spinOnce() checks if received publication, if so, call cb()
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   for(int stepperVal=0; stepperVal < 256; stepperVal++){
-    analogWrite(trans_base_out, stepperVal);
-    //delay(delay_fast);
-    delay(delay_slow_down);
-  }
+    nh.spinOnce();
 
+    while(latest_color == "red"){
+      delay(1);
+    }
+    
+    analogWrite(trans_base_out, stepperVal);
+    if(latest_color == "green"){   // green color, usual(fast) speed motor
+      delay(delay_fast);    
+    }else{     // yellow color, slow down motor
+      delay(delay_slow_down);      
+    }
+  }
+    
   for(int stepperVal=255; stepperVal > 0; stepperVal--){
+    nh.spinOnce();
+
+    while(latest_color == "red"){
+      delay(1);
+    }
+    
     analogWrite(trans_base_out, stepperVal);
-    //delay(delay_fast);
-    delay(delay_slow_down);
+    
+    if(latest_color == "green"){   // green color, usual(fast) speed motor
+      delay(delay_fast);    
+    }else{     // yellow color, slow down motor
+      delay(delay_slow_down);      
+    }
   }
 
-  delay(2);
+  delay(1);
 }
