3623ace022510f33572a744bfe573fbd4fb89efc
jordantjh <jordantjh@hotmail.com>
2018-11-19 21:19:35 -0600

    correct color detecting woohoo!

--- ros_opencv_robotics.py
+++ ros_opencv_robotics.py
@@ -6,28 +6,55 @@ import cv2
 #https://code.likeagirl.io/finding-dominant-colour-on-an-image-b4e075f98097
 #next: try to use playstatino eye (try in ubuntu) https://stackoverflow.com/questions/34288653/how-to-operate-an-ps-eye-with-opencv
 
-video_capture = cv2.VideoCapture(0)
+def find_histogram(clt):
+    """
+    create a histogram with k clusters
+    :param: clt
+    :return:hist
+    """
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
 
-while(True):
-		ret, frame = video_capture.read()  #read() returns an image from the vid stream
+    hist = hist.astype("float")
+    hist /= hist.sum()
 
-		img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-		
-		img = img_rgb.reshape((img_rgb.shape[0] * img_rgb.shape[1],3)) #represent as row*column,channel number
-		clt = KMeans(n_clusters=3) #cluster number
-		clt.fit(img)
+    return hist
 
-		# for width in range(480):
-		# 	for height in range(640):
-		# 		print(img_rgb[width][height][0])
+def main():
+	video_capture = cv2.VideoCapture(0)
+	counter = 1
 
-		#r, g, b = frame.split(img_rgb)
+	while(True):
+			ret, frame = video_capture.read()  #read() returns an image from the vid stream
 
-		cv2.imshow("Frame", frame)
-		if cv2.waitKey(100) & 0xFF == ord('q'):
-			break
+			img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+			
+			img = img_rgb.reshape((img_rgb.shape[0] * img_rgb.shape[1],3)) #represent as row*column,channel number
+			clt = KMeans(n_clusters=1) #cluster number
+			clt.fit(img)
 
-print(clt.cluster_centers_)
+			hist = find_histogram(clt)
 
-video_capture.release()
-cv2.destroyAllWindows()
+			print("counter = " + str(counter))
+			for (percent, color) in zip(hist, clt.cluster_centers_):
+				r = color[0]
+				g = color[1]
+				b = color[2]
+				
+				if r>80 and r<120 and g>135 and b>90 and b<140 :   # check if green
+					print("green!")
+				elif r>115 and r<175 and g>125 and g<180 and b<75 :    # check if yellow
+					print("yellow!")
+				elif r>190 and g>75 and g<120 and b>65 and b<95 :  #check if red
+					print("Red!")
+				else:
+					print("ignored!")
+
+			cv2.imshow("Frame", frame)
+			if cv2.waitKey(100) & 0xFF == ord('q'):
+				break
+
+	video_capture.release()
+	cv2.destroyAllWindows()
+
+main()
