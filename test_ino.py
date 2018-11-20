fc937304041a0d8d8335ef5862e2930c32ddf31f
jordantjh <jordantjh@hotmail.com>
2018-11-20 16:26:30 -0600

    updated test_ino and .ino

--- test_ino.py
+++ test_ino.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python
 
+# may need: chmod +x test_ino.py
+
 import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('command_card_t', String, queue_size=10)  # command_card_t is the topic name
+    pub = rospy.Publisher('command_card_t', String, queue_size=3)  # command_card_t is the topic name
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(1) # 1hz
 
-    counter = 0
+    #counter = 0
     color_detected = "green"
-    while counter < 20:
+    while not rospy.is_shutdown():
         rospy.loginfo(color_detected)
         pub.publish(color_detected)
         rate.sleep()
-        counter += 1
+        #counter += 1
 
-    counter = 0
-    color_detected = "yellow"
-    while counter < 20:
-        rospy.loginfo(color_detected)
-        pub.publish(color_detected)
-        rate.sleep()
-        counter += 1
+    # counter = 0
+    # color_detected = "yellow"
+    # while counter < 20:
+    #     rospy.loginfo(color_detected)
+    #     pub.publish(color_detected)
+    #     rate.sleep()
+    #     counter += 1
 
-    counter = 0
-    color_detected = "red"
-    while counter < 20:
-        rospy.loginfo(color_detected)
-        pub.publish(color_detected)
-        rate.sleep()
-        counter += 1
+    # counter = 0
+    # color_detected = "red"
+    # while counter < 20:
+    #     rospy.loginfo(color_detected)
+    #     pub.publish(color_detected)
+    #     rate.sleep()
+    #     counter += 1
 
 if __name__ == '__main__':
     try:
--- varyingMotorSpeed/varyingMotorSpeed.ino
+++ varyingMotorSpeed/varyingMotorSpeed.ino
@@ -1,21 +1,28 @@
+
 #include <ros.h>
-#include <std_msgs/String.h>
+#include <std_msgs/String.h>   //sudo chmod a+rw /dev/ttyUSB0
+
 
 #define delay_fast 2
 #define delay_slow_down 7
+#define motor_signal 5
 
 ros::NodeHandle  nh;
-String latest_color = "green";  // default to "green" as well
+String latest_color = "red";  // default to "green" as well
 int cur_stepper_val;
 
 void messageCb( const std_msgs::String& color_published){
-  latest_color = color_published;   // blink the led
+  latest_color = color_published.data;   // blink the led
 }
+
+ros::Subscriber<std_msgs::String> sub("command_card_t", &messageCb); // command_card_t is the topic to subscribe to
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(57600);
+  pinMode(motor_signal, OUTPUT);
   nh.initNode();
-  nh.subscribe(command_card_t);   // command_card_t is the topic to subscribe to
+  nh.subscribe(sub);   
 }
 
 //spinOnce() checks if received publication, if so, call cb()
@@ -28,7 +35,7 @@ void loop() {
       delay(1);
     }
     
-    analogWrite(trans_base_out, stepperVal);
+    analogWrite(motor_signal, stepperVal);
     if(latest_color == "green"){   // green color, usual(fast) speed motor
       delay(delay_fast);    
     }else{     // yellow color, slow down motor
@@ -43,7 +50,7 @@ void loop() {
       delay(1);
     }
     
-    analogWrite(trans_base_out, stepperVal);
+    analogWrite(motor_signal, stepperVal);
     
     if(latest_color == "green"){   // green color, usual(fast) speed motor
       delay(delay_fast);    
